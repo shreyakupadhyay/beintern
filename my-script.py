bdcec6d4098bcc1636a221e42677998c5342bf0b
Shreyak Upadhyay <shreyakupadhyay07@gmail.com>
2016-10-27 02:59:47 +0530

    Update my-script.py

--- my-script.py
+++ my-script.py
@@ -5,9 +5,9 @@ Subject : getting data from internshala .
 Description:
 Getting data from internshala website. 
 '''
-import requests
+import requests, sys
 from bs4 import BeautifulSoup
-import sys
+
 
 salary = []
 pages = int(sys.argv[1])
@@ -15,32 +15,26 @@ pages = int(sys.argv[1])
 
 s = requests.session()
 
-# for i in range(0, pages):
-#    soup = s.get('')
-#for i in range(1, pages):
-    #try:
-soup = s.get("http://internshala.com/internships/computer%20science-internship").text
-#soup = s.get('http://internshala.com/internships/page-' + str(i)).text
-html = BeautifulSoup(soup,'lxml')
+def getInternship():
+	soup = s.get("http://internshala.com/internships/computer%20science-internship").text
+	response = BeautifulSoup(soup,'lxml')
+	jobs = response.findAll('div', {'class': 'container-fluid individual_internship'})
 
-jobs = html.findAll('div', {'class': 'container-fluid individual_internship'})
-#print jobs[0]
-#print jobs
-for val in jobs:
-    """
-    This part is for "individual_internship_header" 
-    """
-    #internship title
-    print (val.findChildren()[0]).find('h4').get('title')
-    #print (val.findChildren()[0]).find('h4').getText()
-    print " ".join((val.findChildren()[0]).find('h4').getText().split())  # removing extra spaces from the string
-    print (val.findChildren()[0]).find('h4').findNextSibling('h4').findChildren()[0]['href']
-    print (val.findChildren()[0]).find('h4').findNextSibling('h4').findChildren()[0]['title']
-    """
-    This part is for "individual_internship_details"
-    """ 
-    #Duration , Stipend , Posted On , Apply By
-    for giv in val.findAll('td'):
-        print " ".join(giv.getText().split())
+	for val in jobs:
+	    """
+	    This part is for "individual_internship_header" 
+	    """
+	    #internship title
+	    print (val.findChildren()[0]).find('h4').get('title')
+	    #print (val.findChildren()[0]).find('h4').getText()
+	    print " ".join((val.findChildren()[0]).find('h4').getText().split())  # removing extra spaces from the string
+	    print (val.findChildren()[0]).find('h4').findNextSibling('h4').findChildren()[0]['href']
+	    print (val.findChildren()[0]).find('h4').findNextSibling('h4').findChildren()[0]['title']
+	    """
+	    This part is for "individual_internship_details"
+	    """ 
+	    #Duration , Stipend , Posted On , Apply By
+	    for giv in val.findAll('td'):
+	        print " ".join(giv.getText().split())
 
-    print "GOT+++++++++++++++++++++++++++++++++"
+getInternship()   
